0cd9eb8b6a4ea5cba1df229a4a2a68ca0909c1f7
1F10230306 <s1f102303068@iniad.org>
2024-07-11 14:39:09 +0900

    更新機能の追加

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
-# Create your views here.
+def update (request, task_id):
+    try:
+        task=Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    if request.method == 'POST':
+        task.title = request.POST['title']
+        task.due_at=make_aware (parse_datetime(request.POST['due_at']))
+        task.save()
+        return redirect(detail, task_id)
+    context = {
+        'task': task
+    } 
+    return render (request, "todo/edit.html", context)
 
 
 def index(request):
