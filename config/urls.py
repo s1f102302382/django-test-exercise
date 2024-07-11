ecc68662bb6e910461dfd757559ba8e7a339b207
1F10230306 <s1f102303068@iniad.org>
2024-07-11 19:30:50 +0900

    コンフリクトの解消M

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from todo import views as todo_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', todo_views.index, name='index'),
-    path('<int:task_id>/', todo_views.detail, name='detail'),
-    path('<int:task_id>/close', todo_views.close, name='close'),
+    path("admin/", admin.site.urls),
+    path("", todo_views.index, name="index"),
+    path("<int:task_id>/", todo_views.detail, name="detail"),
+    path("<int:task_id>/close", todo_views.close, name="close"),
+    path("<int:task_id>/update", todo_views.update, name="update"),
 ]
